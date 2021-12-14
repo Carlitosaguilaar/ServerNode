11888fe5b16e9515c92a7a861109f8a741cd4e97
Carlitosaguilaar <caa0005@alu.medac.es>
2021-12-14 13:04:28 +0100

    Uff chungo

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const morgan=require('morgan');
+
+//Configuraciones
+app.set('port', process.env.PORT || 3000);
+app.set('json spaces', 2)
+
+app.get('/', (req, res) => {    
+    res.json(
+        {
+            "Title": "Hola mundo"
+        }
+    );
+})
+
+//Iniciando el servidor, escuchando...
+app.listen(app.get('port'),()=>{
+    console.log(`Server listening on port ${app.get('port')}`);
+});
+
