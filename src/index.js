0e28c1f44b51ccdafc7d818e43489b89ff941726
Carlitosaguilaar <caa0005@alu.medac.es>
2022-01-11 14:39:33 +0100

    Primeras tres clausulas ampliación

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ var con = mysql.createConnection({
 });
 
 con.connect(function(err) {
-
+  
   if (err) throw err;
   console.log("Connected!");
 });
@@ -17,9 +17,11 @@ con.connect(function(err) {
 
 var express = require('express');
 var app = express();
+var server = app.listen(3000,function(err,re) {});
+
 
 //Mostar lista de usuarios
-app.get('/usuarios',function(req,res){
+app.get('/lista',function(req,res){
 
     const sql = "SELECT * from usuarios";
 
@@ -33,7 +35,7 @@ app.get('/usuarios',function(req,res){
 });
 
 //Mostar información de un usuario, filtrando por su ID
-app.get('/usuario',function(req,res){
+app.get('/usuarios',function(req,res){
 
   const id = req.query.id_usuario;
   const sql = "SELECT * from usuarios where ID_Usuario = "+id;
@@ -47,4 +49,69 @@ app.get('/usuario',function(req,res){
   });
 });
 
-var server = app.listen(3000,function(err,re) {});
+
+
+//Lista de vehículos filtrando por ID de usuario
+
+app.get('/vehiculo_usuario',function(req,res){
+
+  const id = req.query.id_usuario;
+  const sql = "SELECT * from vehiculos where Id_usuario = "+id;
+
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+
+    console.log("Result: " + JSON.stringify(result,null,2));
+
+    res.json(result);
+  });
+});
+
+//Información de un vehículo filtrando por el ID del vehículo
+
+app.get('/vehiculo_id',function(req,res){
+
+  const id = req.query.id_vehiculo;
+  const sql = "SELECT * from vehiculos where ID_Vehiculo = "+id;
+
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+
+    console.log("Result: " + JSON.stringify(result,null,2));
+
+    res.json(result);
+  });
+});
+
+
+//Lista de servicios filtrando por un ID de vehículo
+
+app.get('/servicios_vehiculo',function(req,res){
+
+  const id = req.query.id_vehiculo;
+  const sql = "SELECT * from servicios where ID_vehiculo = "+id;
+
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+
+    console.log("Result: " + JSON.stringify(result,null,2));
+
+    res.json(result);
+  });
+});
+
+//Información de un servicio filtrando por el ID del servicio
+
+app.get('/servicios_vehiculo',function(req,res){
+
+  const id = req.query.id_vehiculo;
+  const sql = "SELECT descripcion from servicios where ID_servicio = "+id;
+
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+
+    console.log("Result: " + JSON.stringify(result,null,2));
+
+    res.json(result);
+  });
+});
